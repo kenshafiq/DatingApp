c98c924c21e88cb46c7a6395bda219fbe6ab2a7a
kenshafiq <kenshafiq@gmail.com>
2020-12-02 17:15:32 -0500

    End of Section 5

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,14 +5,30 @@ import { AppComponent } from './app.component';
 import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, HttpClientModule, BrowserAnimationsModule
+    AppRoutingModule, HttpClientModule, BrowserAnimationsModule,
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
